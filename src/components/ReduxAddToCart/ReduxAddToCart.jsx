0bc6a314b95b335f6e3f6c175739824910c7bbd2
VinayReddy18 <142021280+VinnuReddy18@users.noreply.github.com>
2024-06-24 11:47:20 +0530

    added redux , add to cart and remove func

--- src/components/ReduxAddToCart/ReduxAddToCart.jsx
+++ src/components/ReduxAddToCart/ReduxAddToCart.jsx
@@ -1,12 +1,21 @@
-function AddToCart({ product }) {
-    console.log("add to cart", product.id)
+import { useSelector, useDispatch } from "react-redux";
+function ReduxAddToCart({ product }) {
+    console.log("add to cart", product.id);
+    // useSelector
+    let dispatch = useDispatch();
     function increase() {
         //dispatch({type: , payload: })
+        dispatch({ type: "ADD_TO_CART", payload: product });
     }
     function decrease() {
      //dispatch({type: , payload: })
+     dispatch({ type: "REMOVE_FROM_CART", payload: product });
     }
-    const quantity = cart[product.id] ? cart[product.id].quantity : 0;
+
+    let quantity = useSelector((state) => {
+        return state.items[product.id]?.quantity || 0;
+    })
+
     if (quantity === 0) {
         return (
             <div>
@@ -23,7 +32,7 @@ function AddToCart({ product }) {
         )
     }
 }
-export default AddToCart;
+export default ReduxAddToCart;
 
 
 // array of object 
